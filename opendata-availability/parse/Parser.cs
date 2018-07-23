05e5b40db94545911afc17b0e53cd21d7a8e3176
Petrovich.Andrey <petrovich.andrey@wildberries.ru>
2018-07-23 08:43:05 +0300

    async

---
+++ opendata-availability/parse/Parser.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace parse
+{
+    public class Parser
+    {
+        public static async Task Start(string site)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = await web.LoadFromWebAsync(site + @"/opendata");
+
+            var tables = from tbl in htmlDoc.DocumentNode.SelectNodes("//table")
+                         let innerText = tbl.InnerText.ToLower()
+                         where innerText.Contains("набор") && innerText.Contains("данных")
+                         select tbl;
+            var table = tables.FirstOrDefault();
+
+            if (table == null)
+            {
+                Console.WriteLine("talbe not found");
+                return;
+            }
+
+            var rows = table.Descendants("tr").ToList();
+            var links = new List<HtmlNode>();
+
+            if (rows.Count < 2)
+            {
+                Console.WriteLine("Empty table");
+                return;
+            }
+            for (var i = 1; i < rows.Count; i++) // the first row (rows[0]) is a header. Skip it.
+            {
+                var a = rows[i].Descendants("a").FirstOrDefault();
+                if (a != null)
+                    links.Add(a);
+            }
+            //.Where(a => a.GetAttributeValue("href", "").Contains("opendata"));
+
+            var dataLinkCount = 0;
+            var structrueDescrCount = 0;
+            foreach (var n in links)
+            {
+                //Console.WriteLine(n.GetAttributeValue("href", "not-found"));
+                var href = n.GetAttributeValue("href", string.Empty);
+                if (href == string.Empty)
+                    continue;
+
+                var dataPage = site + href;
+                var htmlWeb = new HtmlWeb();
+                var htmlDataPage = await htmlWeb.LoadFromWebAsync(dataPage);
+                var dataLink = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr => tr.InnerText.ToLower().Contains("гиперссылка"))?.Descendants("a").FirstOrDefault();
+                Console.WriteLine(dataPage);
+                if (dataLink != null)
+                {
+                    Console.WriteLine(dataLink.GetAttributeValue("href", string.Empty));
+                    dataLinkCount++;
+                }
+
+                var structureDescrLink = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr =>
+                {
+                    return tr.Descendants("td").FirstOrDefault(td =>
+                    {
+                        var tdInnerText = td.InnerText.ToLower();
+                        return tdInnerText.Contains("описан") && tdInnerText.Contains("структур") && tdInnerText.Contains("данн");
+                    }) != null;
+                })?.Descendants("a").FirstOrDefault();
+
+                if (structureDescrLink != null)
+                {
+                    Console.WriteLine(structureDescrLink.GetAttributeValue("href", string.Empty));
+                    structrueDescrCount++;
+                }
+                else
+                {
+                    var descr = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr =>
+                    {
+                        var innerText = tr.InnerText.ToLower();
+                        return innerText.Contains("описан") && innerText.Contains("структур") &&
+                               innerText.Contains("данн");
+                    });
+                    var a_s = descr.Descendants("a");
+                    var temp = 1;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(links.Count);
+            Console.WriteLine(dataLinkCount);
+            Console.WriteLine(structrueDescrCount);
+        }
+    }
+}
--- opendata-availability/parse/Program.cs
+++ opendata-availability/parse/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Threading.Tasks;
 using System.Xml;
 using HtmlAgilityPack;
 
@@ -9,7 +10,7 @@ namespace parse
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var sites = new List<string>
             {
@@ -22,104 +23,7 @@ namespace parse
 
             foreach (var site in sites)
             {
-                HtmlWeb web = new HtmlWeb();
-                var htmlDoc = web.Load(site + @"/opendata");
-
-                var tables = from tbl in htmlDoc.DocumentNode.SelectNodes("//table")
-                            let innerText = tbl.InnerText.ToLower()
-                            where innerText.Contains("набор") && innerText.Contains("данных")
-                            select tbl;
-                var table = tables.FirstOrDefault();
-
-                if (table == null)
-                {
-                    Console.WriteLine("talbe not found");
-                    return;
-                }
-
-                var rows = table.Descendants("tr").ToList();
-                var links = new List<HtmlNode>();
-
-                if (rows.Count < 2)
-                {
-                    Console.WriteLine("Empty table");
-                    return;
-                }
-                for (var i = 1; i < rows.Count; i++) // the first row (rows[0]) is a header. Skip it.
-                {
-                    var a = rows[i].Descendants("a").FirstOrDefault();
-                    if (a != null)
-                        links.Add(a);
-                }
-                //.Where(a => a.GetAttributeValue("href", "").Contains("opendata"));
-
-                var dataLinkCount = 0;
-                var structrueDescrCount = 0;
-                foreach (var n in links)
-                {
-                    //Console.WriteLine(n.GetAttributeValue("href", "not-found"));
-                    var href = n.GetAttributeValue("href", string.Empty);
-                    if (href == string.Empty)
-                        continue;
-
-                    var dataPage = site + href;
-                    var htmlWeb = new HtmlWeb();
-                    var htmlDataPage = htmlWeb.Load(dataPage);
-                    var dataLink = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr => tr.InnerText.ToLower().Contains("гиперссылка"))?.Descendants("a").FirstOrDefault();
-                    Console.WriteLine(dataPage);
-                    if (dataLink != null)
-                    {
-                        Console.WriteLine(dataLink.GetAttributeValue("href", string.Empty));
-                        dataLinkCount++;
-                    }
-                        
-                    var structureDescrLink = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr =>
-                    {
-                        return tr.Descendants("td").FirstOrDefault(td =>
-                        {
-                            var tdInnerText = td.InnerText.ToLower();
-                            return tdInnerText.Contains("описан") && tdInnerText.Contains("структур") && tdInnerText.Contains("данн");
-                        }) != null;
-                    })?.Descendants("a").FirstOrDefault();
-
-                    if (structureDescrLink != null)
-                    {
-                        Console.WriteLine(structureDescrLink.GetAttributeValue("href", string.Empty));
-                        structrueDescrCount++;
-                    }
-                    else
-                    {
-                        var descr = htmlDataPage.DocumentNode.SelectNodes("//tr").FirstOrDefault(tr =>
-                        {
-                            var innerText = tr.InnerText.ToLower();
-                            return innerText.Contains("описан") && innerText.Contains("структур") &&
-                                   innerText.Contains("данн");
-                        });
-                        var a_s = descr.Descendants("a");
-                        var temp = 1;
-                    }
-                    Console.WriteLine();
-                    /*
-                    var dataRows = from tr in htmlDataPage.DocumentNode.SelectNodes("//tr")
-                                   let innerText = tr.InnerText.ToLower()
-                                   where innerText.Contains("гиперссылка")
-                                   select tr;
-                    var dataLinks = new List<HtmlNode>();
-                    foreach (var row in dataRows)
-                    {
-                        var a = row.Descendants("a").FirstOrDefault();
-                        if (a != null)
-                            dataLinks.Add(a);
-                    }*/
-                    //foreach (var link in dataLinks)
-                    //    Console.WriteLine(link.GetAttributeValue("href", string.Empty));
-                    
-
-                    
-                }
-                Console.WriteLine(links.Count);
-                Console.WriteLine(dataLinkCount);
-                Console.WriteLine(structrueDescrCount);
+                await Parser.Start(site);
             }
             
             Console.ReadLine();
